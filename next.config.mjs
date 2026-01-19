9e1ab5060824d4976d48b6c53f74a7e99c492195
LadyTech-03 <h0555257216@gmail.com>
2026-01-19 08:17:02 +0000

    finally the signature section has been sorted

--- next.config.mjs
+++ next.config.mjs
@@ -12,24 +12,24 @@ const nextConfig = {
   reactStrictMode: false,
 
   // Prevent server processing of these packages
-  // serverExternalPackages: ['@imgly/background-removal', 'onnxruntime-web'],
+  serverExternalPackages: ['@imgly/background-removal', 'onnxruntime-web'],
 
-  // webpack(config, { isServer }) {
-  //   if (!isServer) {
-  //     // Force client to use the browser bundle
-  //     config.resolve.alias['onnxruntime-web$'] = path.join(__dirname, 'node_modules/onnxruntime-web/dist/ort.bundle.min.mjs');
-  //     config.resolve.alias['onnxruntime-web/dist/ort.node.min.mjs'] = false; // Explicitly ignore node bundle
-  //   }
+  webpack(config, { isServer }) {
+    if (!isServer) {
+      // Force client to use the browser bundle
+      config.resolve.alias['onnxruntime-web$'] = path.join(__dirname, 'node_modules/onnxruntime-web/dist/ort.bundle.min.mjs');
+      config.resolve.alias['onnxruntime-web/dist/ort.node.min.mjs'] = false; // Explicitly ignore node bundle
+    }
 
-  //   // ONLY the problematic ORT bundles that Terser chokes on.
-  //   // Do NOT apply to all .mjs in onnxruntime-web, or you can break runtime URL handling.
-  //   config.module.rules.push({
-  //     test: /(ort\.node\.min|ort\.webgpu\.bundle\.min)\.mjs$/,
-  //     type: "javascript/auto",
-  //   });
+    // ONLY the problematic ORT bundles that Terser chokes on.
+    // Do NOT apply to all .mjs in onnxruntime-web, or you can break runtime URL handling.
+    config.module.rules.push({
+      test: /(ort\.node\.min|ort\.webgpu\.bundle\.min)\.mjs$/,
+      type: "javascript/auto",
+    });
 
-  //   return config;
-  // },
+    return config;
+  },
 };
 
 export default nextConfig;
